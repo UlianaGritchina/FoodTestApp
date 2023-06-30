a4fa0e1e963322650d68f7e0f3087bff911db896
Uliana Gritchina <ugritchina@icloud.com>
2023-06-30 19:18:24 +0300

    Initial Commit

---
+++ FoodTestApp/FoodTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTestAppApp.swift
+//  FoodTestApp
+//
+//  Created by Ульяна Гритчина on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
